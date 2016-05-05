90792ab9afcab3ae7b0ec88a3a40e1afbd7966eb
daniel-williams <mail@danielwilliams.us>
2016-05-04 17:57:04 -0700

    Fix: Skyberry needs to spawn in new tab.

--- app/pages/footer/Copyright.jsx
+++ app/pages/footer/Copyright.jsx
@@ -13,7 +13,7 @@ export default React.createClass({
               Copyright <span>&#169;</span> {new Date().getFullYear()}. All rights reserved. Anna Lancaster Fine Art. PO Box 489. Tualatin Oregon 97062-0489.
             </Col>
             <Col xs={12} style={{textAlign:'center'}}>
-              <span className='credits'>With <Icon name='heart-outline' /> by <a href='http://skyberrystudio.com'>Skyberry</a></span>
+              <span className='credits'>With <Icon name='heart-outline' /> by <a href='http://skyberrystudio.com' target='_blank'>Skyberry</a></span>
             </Col>
           </Row>
         </Grid>
